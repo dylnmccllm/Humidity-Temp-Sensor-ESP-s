6915b777d2471ce7f35fed23dbf5a819c9c649ba
Dylan McCollum <dylnmccllm@gmail.com>
2026-04-29 17:29:26 -0600

    Added main code without email, should write to screen

--- Reciever_module.py
+++ Reciever_module.py
@@ -1,7 +1,8 @@
 import network
 import time
 import machine
-from machine import Pin
+import ssd1306
+from machine import Pin, SoftI2C
 import ubinascii
 from umqtt.robust import MQTTClient
 import sys
@@ -9,11 +10,24 @@ import sys
 ssid = "SSID"
 password = "Password"
 
+# ESP32 Pin assignment 
+i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+
+# screen dimensions
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
 mqtt_broker="broker.hivemq.com"
 client_id = b"esp32_" + ubinascii.hexlify(machine.unique_id())
 temp_topic = b"wyohack/Dylan_McCollum/sensor/temperature"
 humid_topic = b"wyohack/Dylan_McCollum/sensor/humidity"
 
+humid = 0
+temp = 0
+
+
+#WiFi connection function
 def connect_wifi(sta_if, ssid, password):
     print(f"connecting to WiFi network: {ssid}...")
     if not sta_if.isconnected():
@@ -31,3 +45,41 @@ def connect_wifi(sta_if, ssid, password):
     else:
         print("WiFi Connection Failed!")
         return False
+    
+#MQTT callback to update temp and humidity values
+def mqtt_callback(topic, msg):
+    global humid, temp
+    if topic == temp_topic:
+        temp = float(msg.decode())
+    elif topic == humid_topic:
+        humid = float(msg.decode())
+        
+#MQTT connection and main loop
+wlan = network.WLAN(network.STA_IF)
+if connect_wifi(wlan, ssid, password):
+    mqtt_client = None
+    try:
+        mqtt_client = MQTTClient(client_id, mqtt_broker, keepalive = 60)
+        mqtt_client.set_callback(mqtt_callback)
+        mqtt_client.connect()
+        mqtt_client.subscribe(temp_topic)
+        mqtt_client.subscribe(humid_topic)
+        print("Connected to MQTT Client")
+        while True:
+            time.sleep(5)
+            mqtt_client.check_msg()
+            print("message received")
+            oled.fill(0)
+            oled.text('Humidity: ' + str(humid) + '%', 0, 0)
+            oled.text('Temperature: ' + str(temp) + 'F', 0, 10)
+            oled.show()
+    except OSError as e:
+        print(f"MQTT/Network Error: {e}")
+        print("Resetting in 5 seconds...")
+        time.sleep(5)
+        machine.reset()
+    except Exception as e:
+        print(f"MQTT Connection Failed: {e}")
+else:
+    print("Cannot start MQTT client without WiFi connection.")
+print("Script finished.")
